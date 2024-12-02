7edf49b18cb423a45709ffa171021053f3149058
“Fighting-WangSB” <17827531520@163.com>
2024-12-01 22:08:53 -0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,14 @@ int main(int argc, char** argv) {
     while(imagecount < MAX_FRAME_COUNT) {
         std::ostringstream oss;
         std::string imagename;
-        oss << "../photo/" << imagecount+1 << ".png";
+        oss << "/home/wang/wsb_slam/photo/" << imagecount+1 << ".png";
         // oss << "/home/wang/wsb_slam/tum_photo/" << imagecount+1 << ".png";
         imagename = oss.str();
         cv::Mat img = cv::imread(imagename, cv::IMREAD_GRAYSCALE);
 
         std::ostringstream oss1;
         std::string imagename1;
-        oss1 << "../photo/" << imagecount+1 << "_depth.png";
+        oss1 << "/home/wang/wsb_slam/photo/" << imagecount+1 << "_depth.png";
         // oss1 << "/home/wang/wsb_slam/tum_photo/" << imagecount+1 << "_depth.png";
         imagename1 = oss1.str();
         cv::Mat img_depth = cv::imread(imagename1, cv::IMREAD_UNCHANGED);
